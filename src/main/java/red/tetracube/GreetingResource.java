5bc07969ae66b54c173eab88e89bbbb3edc4087d
Davide.Di.Do <davide.dcube@gmail.com>
2024-09-10 21:46:29 +0200

    Activate JWT authentication

--- src/main/java/red/tetracube/GreetingResource.java
+++ src/main/java/red/tetracube/GreetingResource.java
@@ -1,10 +1,12 @@
 package red.tetracube;
 
+import io.quarkus.security.Authenticated;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
+@Authenticated
 @Path("/hello")
 public class GreetingResource {
 
